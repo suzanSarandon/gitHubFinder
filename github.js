4c330636839b1ae3dc3519f8c752cc9b62bf81b2
Phil Galanos <filgalanos@gmail.com>
2020-04-23 11:48:41 +0300

    initial commit

---
+++ github.js
@@ -0,0 +1,22 @@
+class GitHub {
+    constructor() {
+        this.client_id = 'Iv1.e7aa2c027fed1479';
+        this.client_secret = 'ea9cee50c05cf5ac0a52c70afffe643916613175';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
